55cb8e48544129199ffdeaddf245f6fda48244c2
Daniil Pautov <danpautoff@yandex.ru>
2022-08-06 13:07:24 +0300

    Update the index page.

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!'
+                        'К запуску на боевом сервере готов!')
 
 
 def second_page(request):
